a1507bbd496626b5162f43bf2775ab908c0e7641
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2024-06-22 12:45:18 +0200

    Add project files.

---
+++ Switch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Switch
+{
+    public class Program
+    {
+        public enum Color
+        {
+            Red,
+            Green, 
+            Blue,
+            Yellow,
+            black,
+            white
+        }
+        static void Main(string[] args)
+        {
+
+            Color c = (Color)(new Random().Next(0, 5));
+
+            switch (c)
+            {
+                case Color.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Color is Red");
+                    break;
+                case Color.Green:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Color is green");
+                    break;
+                case Color.Blue:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Color Blue");
+                    break;
+                case Color.Yellow:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Color yellow");
+                    break;
+                default:
+                    Console.ResetColor();
+                    Console.WriteLine("Color reset");
+                    break;
+            }
+        }
+    }
+}
